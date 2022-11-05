21f8aed2269a9253df8cdc1b70844a11d3ad8ec4
johnmart19 <johnivan19999@gmail.com>
2022-11-05 14:00:50 +0200

    sm8150-common: Enable Dolby Support and Import needed libs
    Update oneplus_vendor_framework_compatibility_matrix.xml

--- common.mk
+++ common.mk
@@ -123,6 +123,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.oneplus.hardware.display@1.0.vendor
 
+# Dolby Support
+TARGET_USES_MIUI_DOLBY := true
+
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=2500 \
     ro.surface_flinger.set_touch_timer_ms=2500 \
