c97eeb0d217e59928e7d346433e426a19b60db56
Antonino Scordino <a.scordino.07@gmail.com>
2022-11-14 22:25:49 +0100

    sm8150-common: libinit: Syntax and indentation improvements
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I61fc78881d7b8de5295b62525ef39bb804526ce6

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -75,10 +75,10 @@ void vendor_load_properties() {
   int prj_version = stoi(android::base::GetProperty("ro.boot.prj_version", ""));
   int project_name = stoi(android::base::GetProperty("ro.boot.project_name", ""));
   int rf_version = stoi(android::base::GetProperty("ro.boot.rf_version", ""));
-  switch(project_name){
+  switch(project_name) {
     case 18857:
       /* OnePlus 7 */
-      switch (rf_version){
+      switch (rf_version) {
         case 1:
           /* China */
           property_override("ro.product.model", "GM1900");
@@ -99,7 +99,7 @@ void vendor_load_properties() {
       break;
     case 18821:
       /* OnePlus 7 Pro */
-      switch (rf_version){
+      switch (rf_version) {
         case 1:
           /* China */
           property_override("ro.product.model", "GM1910");
@@ -124,7 +124,7 @@ void vendor_load_properties() {
       break;
     case 18865:
       /* OnePlus 7T */
-      switch (rf_version){
+      switch (rf_version) {
         case 1:
           /* China */
           property_override("ro.product.model", "HD1900");
@@ -149,7 +149,7 @@ void vendor_load_properties() {
       break;
     case 19801:
       /* OnePlus 7T Pro */
-      switch (rf_version){
+      switch (rf_version) {
         case 1:
           /* China */
           property_override("ro.product.model", "HD1910");
@@ -173,11 +173,11 @@ void vendor_load_properties() {
       property_override("ro.product.model", "HD1925");
       property_override("persist.radio.multisim.config", "ssss");
       break;
-    }
+  }
 
-    property_override("vendor.boot.prj_version", std::to_string(prj_version).c_str());
-    property_override("vendor.boot.rf_version", std::to_string(rf_version).c_str());
-    property_override("vendor.rf.version", std::to_string(rf_version).c_str());
+  property_override("vendor.boot.prj_version", std::to_string(prj_version).c_str());
+  property_override("vendor.boot.rf_version", std::to_string(rf_version).c_str());
+  property_override("vendor.rf.version", std::to_string(rf_version).c_str());
 
   // dalvikvm props
   load_dalvikvm_properties();
