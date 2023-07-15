b463737afb723ad13fb9ac759611dc2a666e0b72
fazil sheik <root@aospa.fajevzutrxtepos2lrslcmrnyc.bx.internal.cloudapp.net>
2023-07-15 15:06:50 +0000

    Inline Neptune
    Change-Id: Ieed4301c7253bed848cc7e8409974fb70d35867d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,14 +85,18 @@ BOARD_KERNEL_CMDLINE := \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
+    video=vfb \
+    640x400,bpp=32 \
+    memsize=3072000 \
     swiotlb=2048
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
-KERNEL_DEFCONFIG := vendor/sm8150-perf_defconfig
+KERNEL_DEFCONFIG := neptune_defconfig
 KERNEL_CUSTOM_LLVM := true
-KERNEL_FULL_LLVM := true
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
