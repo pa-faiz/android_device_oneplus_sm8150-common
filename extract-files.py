e5de939a0298862dc37e061794ed1a716d578004
Fazil Sheik <fazil.sheik96@gmail.com>
2025-01-25 19:33:45 +0530

    sm8150-coommon: Re-enable Elf check for libOmxCore
    Change-Id: I6e9bb83f973271ed5f36706274641e16996d56dd

--- extract-files.py
+++ extract-files.py
@@ -109,7 +109,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.latency@2.1',
     ): lib_fixup_vendor_suffix,
     (
-        'libOmxCore',
         'libwpa_client',
     ): lib_fixup_remove,
 }
